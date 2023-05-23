3a87b5196fc91892257895764ce7ea25e34ec399
SergeyKotovich <115530111+SergeyKotovich@users.noreply.github.com>
2023-05-23 09:02:41 +0300

    refactor

--- Assets/Scripts/DrawingController.cs
+++ Assets/Scripts/DrawingController.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawingController : MonoBehaviour
 {
    [SerializeField] private LineRenderer _lineRendererPrefab;
    
-   private Color _color;
+   private Color? _color;
    private LineRenderer _line;
    private bool _isDrawing;
    private List<LineRenderer> _lines = new();
@@ -26,18 +27,17 @@ public class DrawingController : MonoBehaviour
    }
    private void Update()
    {
+      if (_color == null)
+      {
+         return;
+      }
       if (!_isDrawing && Input.GetMouseButtonDown(0))
       {
          StartDrawing();
       }
       if (_isDrawing)
       {
-         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-         if (Physics.Raycast(ray, out var hitInfo))
-         {
-            _line.positionCount++;
-            _line.SetPosition(_line.positionCount-1,hitInfo.point);
-         }
+         Drawing() ;
       }
 
       if (_isDrawing && Input.GetMouseButtonUp(0))
@@ -45,14 +45,32 @@ public class DrawingController : MonoBehaviour
          FinishDrawing();
       }
    }
-   
-   
+
+   private void Drawing()
+   {
+      var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      if (!Physics.Raycast(ray, out var hitInfo))
+      {
+         return;
+      }
+      if (_line.positionCount > 0)
+      {
+         var lastPosition = _line.GetPosition(_line.positionCount - 1);
+         if (Vector3.Distance(lastPosition, hitInfo.point) < 0.1)
+         {
+            return;
+         }
+      }
+
+      _line.positionCount++;
+      _line.SetPosition(_line.positionCount - 1, hitInfo.point);
+   }
+
 
    private void StartDrawing()
    {
-      
       _line = Instantiate(_lineRendererPrefab);
-      _line.material.color = _color;
+      _line.material.color = _color.Value;
       _line.sortingOrder = _lines.Count;
       _lines.Add(_line);
       _isDrawing = true;
